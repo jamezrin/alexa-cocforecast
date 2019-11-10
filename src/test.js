eeae5a5a1e0cf6253bb6abfc718115d2c2aa4101
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-11-10 18:16:45 +0100

    resolve dirname path of test

--- src/test.js
+++ src/test.js
@@ -1,10 +1,12 @@
 const provider = require('./provider');
 const fs = require('fs');
+const path = require('path');
 const entities = new (require('html-entities').AllHtmlEntities)();
 
 provider.accessStats().then(function(result) {
-    fs.writeFileSync("samples/response.json", JSON.stringify(result))
+    const samplePath = path.resolve(__dirname, 'samples/response.json');
+    fs.writeFileSync(samplePath, JSON.stringify(result));
 
     const forecastMessage = entities.decode(result.forecastMessages["spanish"]);
-    console.log(forecastMessage)
+    console.log(forecastMessage);
 });
