8a3ac96d662dfdc65caecbf87162e7529d61e92c
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-11-12 23:30:53 +0100

    hopefully pass certification

--- src/languages/spanish.js
+++ src/languages/spanish.js
@@ -1,13 +1,13 @@
 module.exports = {
     translation: {
-        SKILL_NAME: 'Botín de Clash of Clans',
+        SKILL_NAME: 'Botín de CoC',
         GENERIC_LANG: 'spanish',
         ERROR_MESSAGE: 'Ha ocurrido un error, por favor inténtalo otra vez',
         EXIT_MESSAGE: '¡Hasta luego!',
         UPWARDS_TREND: 'subiendo',
         DOWNWARDS_TREND: 'bajando',
         STABLE_TREND: 'manteniéndose ahi por ahora',
-        MAIN_STATS_MESSAGE: 'Bienvenido a Botín de Clash of Clans, el botín está en %s sobre 10 y %s. %s Visita clashofclansforecaster.com para más información.',
+        MAIN_STATS_MESSAGE: 'Bienvenido a Botín de CoC, el botín está en %s sobre 10 y %s. %s Visita clashofclansforecaster.com para más información.',
         HELP_MESSAGE: 'Los datos de esta skill están proporcionados por clashofclansforecaster.com. Puedes ver como funciona esta herramienta visitando su sitio web'
     }
 };
