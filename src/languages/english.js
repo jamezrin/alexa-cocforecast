9bc29b70b324b1d02d509b5dc7d9077e85aa0f73
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-11-16 19:26:43 +0100

    we go agane

--- src/languages/english.js
+++ src/languages/english.js
@@ -1,13 +1,13 @@
 module.exports = {
     translation: {
-        SKILL_NAME: 'CoC Loot Forecaster',
+        SKILL_NAME: 'Loot Forecaster',
         GENERIC_LANG: 'english',
         ERROR_MESSAGE: 'Sorry, an error occurred. Try again later!',
         EXIT_MESSAGE: 'See you next time!',
         UPWARDS_TREND: 'going up',
         DOWNWARDS_TREND: 'going down',
         STABLE_TREND: 'staying there for now',
-        MAIN_STATS_MESSAGE: 'Welcome to CoC Loot Forecaster, currently the loot is at %s out of 10 and %s. %s Visit clashofclansforecaster.com for more information.',
-        HELP_MESSAGE: 'This skill uses the data provided by clashofclansforecaster.com. Visit their website to know more about how it works and get even more data'
+        MAIN_STATS_MESSAGE: 'Welcome to Loot Forecaster, currently the loot is at %s out of 10 and %s. %s Visit clashofclansforecaster.com for more information.',
+        HELP_MESSAGE: 'This skill uses the data provided by clashofclansforecaster.com. Visit their website to know more about how it works and get even more data.'
     }
 };
